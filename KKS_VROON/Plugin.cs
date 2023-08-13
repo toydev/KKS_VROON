cbea317c3dbdb5ed914cc7ca2699e8fb05dc18d4
toydev <toy@ownway.info>
2023-08-13 12:13:11 +0900

    Set up the BepInEx plugin

---
+++ KKS_VROON/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using KKAPI;
+
+namespace KKS_VROON
+{
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInProcess(KoikatuAPI.GameProcessName)]
+    [BepInProcess(KoikatuAPI.StudioProcessName)]
+    [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string GUID = nameof(KKS_VROON);
+        private const string Name = nameof(KKS_VROON);
+        private const string Version = "0.01";
+
+        void Awake()
+        {
+            Logger.LogInfo("Awake");
+        }
+    }
+}
