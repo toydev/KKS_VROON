7d62917c9f06f66ecc93014f1004f811a254b0f4
toydev <toy@ownway.info>
2023-08-23 01:16:24 +0900

    Fixed a bug where menus could not be dragged.

---
+++ KKS_VROON/Patches/HandPatches/EventSystemPatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HarmonyLib;
+using UnityEngine.EventSystems;
+
+namespace KKS_VROON.Patches.HandPatches
+{
+    // Exclude ScreenPointToRayPatch in EventSystem.
+
+    [HarmonyPatch(typeof(EventSystem))]
+    public class EventSystemPatch
+    {
+        [HarmonyPatch(nameof(EventSystem.Update))]
+        [HarmonyPrefix]
+        public static bool PrefixUpdate()
+        {
+            ScreenPointToRayPatch.Enabled = false;
+            return true;
+        }
+
+        [HarmonyPatch(nameof(EventSystem.Update))]
+        [HarmonyFinalizer]
+        public static Exception FinalizerUpdate(Exception __exception)
+        {
+            ScreenPointToRayPatch.Enabled = true;
+            return __exception;
+        }
+
+        [HarmonyPatch(nameof(EventSystem.RaycastAll), new Type[] { typeof(PointerEventData), typeof(List<RaycastResult>) })]
+        [HarmonyPrefix]
+        public static bool PrefixRaycastAll()
+        {
+            ScreenPointToRayPatch.Enabled = false;
+            return true;
+        }
+
+        [HarmonyPatch(nameof(EventSystem.RaycastAll), new Type[] { typeof(PointerEventData), typeof(List<RaycastResult>) })]
+        [HarmonyFinalizer]
+        public static Exception FinalizerRaycastAll(Exception __exception)
+        {
+            ScreenPointToRayPatch.Enabled = true;
+            return __exception;
+        }
+    }
+}
