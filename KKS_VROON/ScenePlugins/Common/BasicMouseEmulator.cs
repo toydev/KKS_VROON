ff4f22b3c8b5d737d7c52446361e68a49e11dc09
toydev <toy@ownway.info>
2023-09-03 10:02:50 +0900

    Removed the following IMouseEmulator methods. - GetMouseButton - GetMouseButtonDown - GetMouseButtonUp

--- KKS_VROON/ScenePlugins/Common/BasicMouseEmulator.cs
+++ KKS_VROON/ScenePlugins/Common/BasicMouseEmulator.cs
@@ -22,54 +22,6 @@ namespace KKS_VROON.ScenePlugins.Common
             return null;
         }
 
-        public virtual bool? GetMouseButton(int button)
-        {
-            if (!HandController) return null;
-
-            switch (button)
-            {
-                // Left button
-                case 0: return HandController.State.IsTriggerOn;
-                // right button
-                case 1: return HandController.State.IsGripOn;
-                // middle button
-                case 2: return HandController.State.IsButtonXOn || HandController.State.IsButtonAOn;
-                default: return null;
-            }
-        }
-
-        public virtual bool? GetMouseButtonDown(int button)
-        {
-            if (!HandController) return null;
-
-            switch (button)
-            {
-                // Left button
-                case 0: return HandController.State.IsTriggerDown;
-                // right button
-                case 1: return HandController.State.IsGripDown;
-                // middle button
-                case 2: return HandController.State.IsButtonXDown || HandController.State.IsButtonADown;
-                default: return null;
-            }
-        }
-
-        public virtual bool? GetMouseButtonUp(int button)
-        {
-            if (!HandController) return null;
-
-            switch (button)
-            {
-                // Left button
-                case 0: return HandController.State.IsTriggerUp;
-                // right button
-                case 1: return HandController.State.IsGripUp;
-                // middle button
-                case 2: return HandController.State.IsButtonXUp || HandController.State.IsButtonAUp;
-                default: return null;
-            }
-        }
-
         public virtual void SendMouseEvent()
         {
             if (HandController.State.IsTriggerUp) MouseKeyboardUtils.MouseLeftUp();
